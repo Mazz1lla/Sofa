12f566f16a293e8c67ef12018a3b4b6862746415
Mazz1lla <123858320+Mazz1lla@users.noreply.github.com>
2025-08-01 04:37:46 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import io
 import requests
 from flask import Flask, request, send_file, jsonify
-from elevenlabs import generate, set_api_key
+from elevenlabs.client import ElevenLabs
 from dotenv import load_dotenv
 
 load_dotenv()
